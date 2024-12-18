f9bb405ee8e58bf4be6dcf860a126f3d76376897
kijudev <dev@kiju.page>
2024-12-18 16:48:28 +0100

    Add docker

--- flake.nix
+++ flake.nix
@@ -1,14 +1,24 @@
 {
-    description = "personal-website-flake";
+  description = "blueprint-flake";
 
-    inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
 
-    outputs = { self, nixpkgs, ... }@inputs:
-        let
-            system = "x86_64-linux";
-            pkgs = nixpkgs.legacyPackages.${system};
-        in {
-        devShells.${system}.default =
-        pkgs.mkShell { nativeBuildInputs = with pkgs; [ go gopls docker ]; };
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = system;
+        config.allowUnfree = true;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          go
+          gopls
+          docker
+          zed-editor
+          jetbrains.datagrip
+        ];
+      };
     };
 }
