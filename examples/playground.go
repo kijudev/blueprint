a686551ec94c5c66f793e774aceb9fe53a57eeeb
kijudev <dev@kiju.page>
2024-12-27 01:32:39 +0100

    Begin work on the signal system

--- examples/playground.go
+++ examples/playground.go
@@ -4,32 +4,43 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/kijudev/blueprint/lib"
-	"github.com/kijudev/blueprint/modules/authpg"
-	"github.com/kijudev/blueprint/modules/dbpg"
+	"github.com/kijudev/blueprint/signals"
 )
 
 func main() {
-	ctx := context.Background()
+	// ctx := context.Background()
 
-	dbpgModule := dbpg.New(dbpg.ModuleConfig{
-		ConnStr: "postgresql://blueprint:1234@localhost:5432/blueprint",
+	// dbpgModule := dbpg.New(dbpg.ModuleConfig{
+	// 	ConnStr: "postgresql://blueprint:1234@localhost:5432/blueprint",
+	// })
+	// dbpgModule.MustInit(ctx)
+	// defer dbpgModule.MustStop(ctx)
+
+	// authModule := authpg.New(authpg.ModuleDeps{
+	// 	DB: dbpgModule.DBService(),
+	// })
+	// authModule.MustInit(ctx)
+	// defer authModule.MustStop(ctx)
+
+	// //user, err := authModule.DataService().GetAccountByID(ctx, lib.MustNewID("0193f7da-6cd3-fb6e-f33a-5f7b4f8f8103"))
+	// user, err := authModule.DataService().GetAccounts(ctx, lib.Pagination{Offset: 0, Limit: 10})
+
+	// if err != nil {
+	// 	panic(err)
+	// } else {
+	// 	fmt.Println(user)
+	// }
+
+	s := signals.New[int]()
+
+	s.Listen(func(ctx context.Context, e signals.Event[int]) {
+		fmt.Println("1 ", e.Data)
 	})
-	dbpgModule.MustInit(ctx)
-	defer dbpgModule.MustStop(ctx)
-
-	authModule := authpg.New(authpg.ModuleDeps{
-		DB: dbpgModule.DBService(),
+	s.Listen(func(ctx context.Context, e signals.Event[int]) {
+		fmt.Println("2 ", e.Data)
 	})
-	authModule.MustInit(ctx)
-	defer authModule.MustStop(ctx)
 
-	//user, err := authModule.DataService().GetAccountByID(ctx, lib.MustNewID("0193f7da-6cd3-fb6e-f33a-5f7b4f8f8103"))
-	user, err := authModule.DataService().GetAccounts(ctx, lib.Pagination{Offset: 0, Limit: 10})
-
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println(user)
-	}
+	s.Dispatch(42)
+	s.Dispatch(43)
+	s.Dispatch(44)
 }
